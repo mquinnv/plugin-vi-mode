f2b19c3660657ebc2846de8ce5a06fce5029f535
Michael Ventura <michael@aluminum.local>
2015-10-01 14:52:52 -0400

    change python file location

--- vi-mode.fish
+++ vi-mode.fish
@@ -48,7 +48,7 @@ end
 
 function __vi_mode_direction_command
     # commandline should always be last
-    set ret (python (dirname (status -f))/vi-mode-impl.py $argv[1] $argv[2] (commandline -C) (commandline -L) (commandline))
+    set ret (python (dirname (status -f))/pkg/vi-mode/vi-mode-impl.py $argv[1] $argv[2] (commandline -C) (commandline -L) (commandline))
     set new_pos $ret[-1]
     set -e ret[-1] # Guessing that deleting last element is likely to be faster than deleting first
     if test (count $ret) -gt 0
